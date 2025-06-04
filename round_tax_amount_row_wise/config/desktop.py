7a6fb68c0813800633106cb589358106cb0030f7
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-06-04 17:22:23 +0300

    feat(pre-commit): setup pre-commit (#1)
    * feat(pre-commit): setup pre-commit
    
    * feat(init): update version
    
    * feat(config): setup pre-commit

--- round_tax_amount_row_wise/config/desktop.py
+++ round_tax_amount_row_wise/config/desktop.py
@@ -1,10 +1,11 @@
 from frappe import _
 
+
 def get_data():
 	return [
 		{
 			"module_name": "Round Tax Amount Row Wise",
 			"type": "module",
-			"label": _("Round Tax Amount Row Wise")
+			"label": _("Round Tax Amount Row Wise"),
 		}
 	]
