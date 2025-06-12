b3ec10ed0c304b0e2531ef0dc3cff234d42cf62e
Alaa Alsalehi <alaadiaa@gmail.com>
2025-06-13 00:57:28 +0400

    fix: move to init of override package to be sure any call to this file will be after classes load

--- round_tax_amount_row_wise/__init__.py
+++ round_tax_amount_row_wise/__init__.py
@@ -1,9 +1,2 @@
 __version__ = "0.0.6"
 
-from round_tax_amount_row_wise.override.override_doctype_class_having_accounts_controller import (
-	get_override_doctype_class,
-)
-try:
-    override_doctype_class = get_override_doctype_class()
-except :
-    pass
