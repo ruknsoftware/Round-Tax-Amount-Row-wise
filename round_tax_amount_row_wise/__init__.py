6e91b5b25dc939e3ec95054df47069439859f290
Alaa Alsalehi <alaadiaa@gmail.com>
2025-06-12 23:24:42 +0400

    fix: try to be sure dynamic classed created

--- round_tax_amount_row_wise/__init__.py
+++ round_tax_amount_row_wise/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.0.4"
+__version__ = "0.0.5"
+
+from round_tax_amount_row_wise.override.override_doctype_class_having_accounts_controller import (
+	get_override_doctype_class,
+)
+
+override_doctype_class = get_override_doctype_class()
