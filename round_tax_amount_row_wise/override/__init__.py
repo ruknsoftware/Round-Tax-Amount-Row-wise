a0ca38cbb87fc2890f6b3b17ca7a4411fbf79aaa
Alaa Alsalehi <alaadiaa@gmail.com>
2025-09-13 01:47:00 +0300

    fix: tax issue for inclusive tax (#6)
    * fix: tax issue for inclusive tax
    
    * build: change version
    
    * build: linter issue
    
    * build: linter issue
    
    * build: linter issue
    
    * build: linter issue

--- round_tax_amount_row_wise/override/__init__.py
+++ round_tax_amount_row_wise/override/__init__.py
@@ -1,7 +1,8 @@
 from round_tax_amount_row_wise.override.override_doctype_class_having_accounts_controller import (
 	get_override_doctype_class,
 )
+
 try:
-    override_doctype_class = get_override_doctype_class()
-except :
-    pass
+	override_doctype_class = get_override_doctype_class()
+except Exception:
+	pass
