adfea0bad204f3a1309163fd63be9788771fc28b
Alaa Alsalehi <alaadiaa@gmail.com>
2025-09-13 02:40:02 +0400

    build: linter issue

--- round_tax_amount_row_wise/override/__init__.py
+++ round_tax_amount_row_wise/override/__init__.py
@@ -1,6 +1,7 @@
 from round_tax_amount_row_wise.override.override_doctype_class_having_accounts_controller import (
 	get_override_doctype_class,
 )
+
 try:
 	override_doctype_class = get_override_doctype_class()
 except:
